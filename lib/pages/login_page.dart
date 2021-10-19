6151e450d556004d1616539c0a9cdf025faef486
Sabbir Hossain <sabbir.sec.cse@gmail.com>
2021-10-19 22:59:36 +0600

    Animated Container , stateful widget e.t.c

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_application_demo/Utilities/MyRoutes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changed = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -17,7 +26,7 @@ class LoginPage extends StatelessWidget {
             ),
             SizedBox(height: 24),
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
             ),
             SizedBox(height: 10),
@@ -28,6 +37,10 @@ class LoginPage extends StatelessWidget {
                   TextFormField(
                     decoration: InputDecoration(
                         hintText: "Enter Your Name", labelText: "User Name"),
+                    onChanged: (text) {
+                      name = text;
+                      setState(() {});
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -37,14 +50,42 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 20,
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      print("Hi CodePur");
+                  InkWell(
+                    onTap: () async {
+                      changed = true;
+                      setState(() {});
+                      await Future.delayed(Duration(seconds: 1));
                       Navigator.pushNamed(context, MyRouts.homeRoute);
                     },
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(),
+                    child: AnimatedContainer(
+                      width: changed ? 50 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: changed
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                      decoration: BoxDecoration(
+                          borderRadius: changed
+                              ? BorderRadius.circular(50.0)
+                              : BorderRadius.circular(8.0),
+                          color: Colors.deepPurpleAccent),
+                      duration: Duration(seconds: 1),
+                    ),
                   )
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     print("Hi CodePur");
+                  //     Navigator.pushNamed(context, MyRouts.homeRoute);
+                  //   },
+                  //   child: Text("Login"),
+                  //   style: TextButton.styleFrom(),
+                  // )
                 ],
               ),
             )
